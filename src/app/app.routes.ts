d9959cd3e11750226706128cbe3d2ca1a3acb6e6
Darren Horrocks <killallthehumans@gmail.com>
2026-06-28 22:25:15 +0100

    disclaimer and privacy policy

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
+import { DisclaimerComponent } from './disclaimer/disclaimer.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'privacy-policy', component: PrivacyPolicyComponent },
+  { path: 'disclaimer', component: DisclaimerComponent },
+];
